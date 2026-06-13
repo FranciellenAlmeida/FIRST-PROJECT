50e6b73f8fa454c681859e2da7760b500a98ea28
FranciellenAlmeida <silvaalmeida.franciellen@gmai.com>
2026-06-12 21:26:01 -0300

    Aprendendo variavéis e imprimir na tela

---
+++ Primeiro-progama.py
@@ -0,0 +1,9 @@
+from builtins import print
+
+nome = "Franciellen"
+sobrenome = "Almeida"
+idade = 23
+altura = 1.54
+maior_de_idade = True
+
+print(f"Olá {nome} {sobrenome} voce tem {idade} anos, sua altura é {altura}, vocé é maior de idade? {maior_de_idade}!")
