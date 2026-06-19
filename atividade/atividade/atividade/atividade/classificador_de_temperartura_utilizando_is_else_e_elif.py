0aff0b90f7043a75810c2d7b5d6373b239786793
Franciellen Almeida da Silva <franciellen64885086@edu.df.senac.br>
2026-06-18 21:17:37 -0300

    Exercicios 18/06/26

---
+++ atividade/atividade/atividade/atividade/classificador_de_temperartura_utilizando_is_else_e_elif.py
@@ -0,0 +1,10 @@
+# Classificador de temperartura utilizando is else e elif
+
+temp = int(input("Coloque a temperatura atual: "))
+
+if temp < 15:
+    print("Clima frio!")
+elif temp < 25:
+    print("Clima Agradáve!")
+else:
+    print("Clima quente!")
